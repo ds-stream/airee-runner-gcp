38ecc8e762181c3e29a63f20a79047ab758b57b3
michalmi-dss <michal.milik@dsstream.com>
2022-06-13 09:24:58 +0200

    change root folder name added gcp secret get in start sh

--- runner-app/start.sh
+++ runner-app/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd ${RUNNER_PATH}
 
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+
+# get PAT_TOKEN from secret menager
+pat_token_version=$(gcloud secrets versions list runner_gh_token --limit=1 --sort-by=name --format="get(name)")
+PAT_TOKEN=$(gcloud secrets versions access ${pat_token_version})
+
 #if ((`echo ${REPO_PROJECT} | wc -m` > 1 )); then
 #TOKEN=$(curl -sX POST -H "Authorization: token ${PAT_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/${REPO_PROJECT}/actions/runners/registration-token | jq .token --raw-output)
 #else
